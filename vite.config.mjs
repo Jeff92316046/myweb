75ee42fc334ed80ecee0bf4d1b4efd238d46e14e
JEFF Liao <jeffliao92316046@gmail.com>
2024-05-15 14:57:58 +0800

    owo

--- vite.config.mjs
+++ vite.config.mjs
@@ -11,7 +11,7 @@ import { fileURLToPath, URL } from 'node:url'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:`/myweb/`,
+  base:`/myweb`,
   plugins: [
     VueRouter(),
     Vue({
